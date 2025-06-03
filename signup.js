6542867641a9abf7b5aa2089527ed0601dd222fd
Damilare Oyinloye <oyinloyedamilare12@gmail.com>
2025-06-03 18:52:53 +0100

    Sign up, sign in and local storage dusted

--- signup.js
+++ signup.js
@@ -4,10 +4,22 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
-const allUsers = [];
+let allUsers = JSON.parse(localStorage.getItem("noters")) || [];
+
+
+// localStorage.setItem('users', JSON.stringify(allUsers)) || []
+
+
+if (localStorage.users) {
+    let retrieved = JSON.parse(localStorage.getItem('allUsers')) || [];
+    console.log(retrieved);
+    allUsers == retrieved
+} else {
+    allUsers == []
+}
+
 
 // localStorage.users = JSON.stringify(allUsers) || [];
-// localStorage.setItem('users', JSON.stringify(allUsers));
 
 const toast = (text, background, color, position = 'right') => {
     Toastify({
@@ -26,20 +38,32 @@ const toast = (text, background, color, position = 'right') => {
     }).showToast();
 }
 
+// const toSignIn = () => {
+//     setTimeout(() => {
+//         window.location.href = 'signin.html';
+//     }, 1500);
+//     toSignIn();
+
+// }
+
+
+
 const signUp = () => {
+    if (password.value !== confirmPass.value) {
+            toast('Password mismatch ', '#f00', '#fff')
+            document.getElementById('confirmPass').value = '';
+        } else{
+            console.log('correct');
+            
+        }
 
     if (fullName.value === '' || email.value === '' || password.value === '' || confirmPass.value === '') {
         toast('Not that quick 😁, please fill in the inputs', '#f00', '#fff')
-        if (password.value !== confirmPass.value) {
-            toast('Password mismatch ', '#f00', '#fff')
-            document.getElementById('confirmPass').value = '';
-        }
-
     } else {
         sub.innerHTML = 'Signing up...'
         setTimeout((() => {
             sub.innerHTML = 'Sign Up';
-        }),1000) 
+        }), 1000)
 
         const fName = document.getElementById('fullName').value;
         const mail = document.getElementById('email').value;
@@ -47,39 +71,36 @@ const signUp = () => {
         const confPass = document.getElementById('confirmPass').value;
 
         const user = { fName, mail, pass };
+        // allUsers.push(user);
+        // console.log(allUsers);
+        // localStorage.setItem("noters", JSON.stringify(allUsers));
+        // console.log(localStorage.getItem("users"));
+        
 
-        let found = allUsers.find(eachUser => eachUser.mail == mail)
-        // console.log(found);
-        if (found == undefined) {
+        let found = allUsers.find(eachUser => eachUser.mail === mail)
+        console.log(found);
+
+        if (found === undefined) {
             allUsers.push(user);
+            toast('Sign up successful, redirecting to sign in...', '#00f', '#fff', 'left');
             console.log(allUsers);
-            toast('Signing up...', '#00f', '#fff', 'left');
 
             // Clear input fields
             document.getElementById('email').value = '';
             document.getElementById('password').value = '';
             document.getElementById('confirmPass').value = '';
 
-            // localStorage.users = JSON.stringify(allUsers)
-            localStorage.setItem("users", JSON.stringify(allUsers));
+            localStorage.noters = JSON.stringify(allUsers)
 
             setTimeout(() => {
-                // window.location.href = 'signin.html';
-            }, 1000);
-            // window.location.href = 'signin.html';
+                window.location.href = 'loaderlog.html';
+            }, 1500);
+
         } else {
             console.log('User already exists');
-            
             toast('User already exists', '#f00', '#fff');
-            setTimeout(()=>{
-                // window.location.href = 'signup.html';
-            },2000)
-
-            document.getElementById('email').value = '';
-            document.getElementById('password').value = '';
-            document.getElementById('confirmPass').value = '';
-
-
+        
         }
     }
 }
+
